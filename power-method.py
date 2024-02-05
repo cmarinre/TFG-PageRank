c1eaa36a9674006f87cfe15385d67307dacf7fca
cmarinre <cmarinre@correo.ugr.es>
2024-02-05 20:15:36 +0100

    first commit

---
+++ power-method.py
@@ -0,0 +1,11 @@
+# Programa para sumar los números del 1 al 100
+
+# Inicializar la variable para almacenar la suma
+suma = 0
+
+# Bucle para sumar los números del 1 al 100
+for numero in range(1, 101):
+    suma += numero
+
+# Imprimir el resultado por consola
+print("La suma de los números del 1 al 100 es:", suma)
